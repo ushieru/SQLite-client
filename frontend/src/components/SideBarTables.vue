dd45e85246c0d8d9effd5e59de49656a331dbc87
ushieru <uzielcocolan@gmail.com>
2024-01-18 13:40:45 -0600

    Feat: add simple snippets

--- frontend/src/components/SideBarTables.vue
+++ frontend/src/components/SideBarTables.vue
@@ -2,6 +2,27 @@
 import { useGlobalStore } from './../stores/GlobalStore'
 
 const store = useGlobalStore()
+
+const onClickAutoQuery = (tableName, action) => {
+    const tab = store.tabs.find(t => t.id == store.currentTabId)
+    switch (action) {
+        case 'C':
+            tab.query = `INSER INTO \`${tableName}\` ()\nVALUES()`
+            break;
+        case 'R':
+            tab.query = `SELECT * FROM \`${tableName}\`\nWHERE ? = ?`
+            break;
+        case 'U':
+            tab.query = `UPDATE \`${tableName}\` SET ? = ?\nWHERE ? = ?`
+            break;
+        case 'D':
+            tab.query = `DELETE FROM \`${tableName}\`\nWHERE ? = ?`
+            break;
+        case 'T':
+            tab.query = `CREATE TRIGGER [IF NOT EXISTS] trigger_name\n[BEFORE|AFTER|INSTEAD OF] [INSERT|UPDATE|DELETE]\nON ${tableName}\n[WHEN condition]\nBEGIN\n\tstatements;\nEND;`
+            break;
+    }
+}
 </script>
 
 <template>
@@ -15,13 +36,15 @@ const store = useGlobalStore()
                 <button class="btn grow" @click="() => store.selectTable(name)">
                     {{ name }}
                 </button>
-                <div v-if="false" class="dropdown">
+                <div class="dropdown">
                     <div tabindex="0" role="button" class="btn">▼</div>
                     <ul tabindex="0" style="position: fixed;"
                         class="dropdown-content z-10 menu p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Create</a></li>
-                        <li><a>Update</a></li>
-                        <li><a>Delete</a></li>
+                        <li><a @click="() => onClickAutoQuery(name, 'C')">Create</a></li>
+                        <li><a @click="() => onClickAutoQuery(name, 'R')">Read</a></li>
+                        <li><a @click="() => onClickAutoQuery(name, 'U')">Update</a></li>
+                        <li><a @click="() => onClickAutoQuery(name, 'D')">Delete</a></li>
+                        <li><a @click="() => onClickAutoQuery(name, 'T')">Trigger</a></li>
                     </ul>
                 </div>
             </div>
